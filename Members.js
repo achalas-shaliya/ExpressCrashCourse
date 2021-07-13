58972439fb95b6e28df33d4c0ba5a6ebd250db93
Achala Shaliya <39802628+achalas-shaliya@users.noreply.github.com>
2021-07-13 22:41:05 +0530

    members_V2
    members_V2

--- Members.js
+++ Members.js
@@ -19,4 +19,25 @@ const members = [
     }
 ];
 
+const members_V2 = [
+    {
+        id: 1,
+        name: 'John Doe',
+        email: 'john@gmail.com',
+        status: 'active'
+    },
+    {
+        id: 2,
+        name: 'Bob Williams',
+        email: 'bob@gmail.com',
+        status: 'inactive'
+    },
+    {
+        id: 3,
+        name: 'Shannon Jackson',
+        email: 'shannon@gmail.com',
+        status: 'active'
+    }
+];
+
 module.exports = members;
